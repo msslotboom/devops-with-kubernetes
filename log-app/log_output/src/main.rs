e381bef41fd247b5e4048aa071441bdb96290473
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-10-06 21:45:51 +0300

    Exercise 1.11

--- log-app/log_output/src/main.rs
+++ log-app/log_output/src/main.rs
@@ -11,7 +11,15 @@ async fn main() -> Result<(), rocket::Error> {
 }
 #[get("/")]
 async fn read_log() -> String {
-    read_to_string("/usr/src/app/files/log.txt")
+    let log = read_to_string("/usr/src/app/files/log.txt")
         .await
-        .unwrap_or_else(|_| "Cannot read log file".to_string())
+        .unwrap_or_else(|_| "Cannot read log file".to_string());
+
+    let pongs = read_to_string("/usr/src/app/files/pongs.txt")
+        .await
+        .unwrap_or_else(|_| "Cannot read pong file".to_string());
+
+    let log_printout = log.lines().last().unwrap_or("Log file is empty.");
+    let pong_printout = "Ping/Pongs: ".to_owned() + &pongs;
+    format!("{}\n{}", log_printout, pong_printout)
 }
--- pingpong/index.js
+++ pingpong/index.js
@@ -1,9 +1,17 @@
 const port = process.env.PORT
+const fs = require('node:fs')
 const express = require('express')
 const app = express()
 
 var pong = 0
-
+fs.readFile('/usr/src/app/files/pongs.txt', 'utf8', (err, data) => {
+	if (err) {
+		console.log(err);
+		writeFile('0');
+		return;
+	}
+	pong = parseInt(data);
+});
 
 const printPort = () => {
 	console.log(`Server started on port ${port}`)
@@ -18,6 +26,15 @@ app.get('/pingpong', (request, response) => {
 	console.log('request: ' + request)
 	response.send('pong ' + pong)
 	pong = pong + 1;
+	writeFile(pong.toString());
 })
+
+const writeFile = (text) => {
+	fs.writeFile('/usr/src/app/files/pongs.txt', text, err => {
+		if (err) {
+			console.log(err);
+		}
+	})
+}
 
 printPort()
