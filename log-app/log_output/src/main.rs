ebdd549e163df5f5b8267ccc4c6a5b6f0e3750b4
Mikael Slotboom <mikael.slotboom@helsinki.fi>
2024-10-28 15:07:05 +0200

    Exercise 2.01

--- log-app/log_output/src/main.rs
+++ log-app/log_output/src/main.rs
@@ -1,5 +1,6 @@
 use rocket::tokio::fs::read_to_string;
 use rocket::{get, routes};
+use reqwest::Error;
 
 #[rocket::main]
 async fn main() -> Result<(), rocket::Error> {
@@ -15,11 +16,18 @@ async fn read_log() -> String {
         .await
         .unwrap_or_else(|_| "Cannot read log file".to_string());
 
-    let pongs = read_to_string("/usr/src/app/files/pongs.txt")
+    let pongs = fetch_pongs()
         .await
-        .unwrap_or_else(|_| "Cannot read pong file".to_string());
+        .unwrap_or_else(|_| "fetching pongs failed".to_string());
 
     let log_printout = log.lines().last().unwrap_or("Log file is empty.");
     let pong_printout = "Ping/Pongs: ".to_owned() + &pongs;
     format!("{}\n{}", log_printout, pong_printout)
+}
+
+async fn fetch_pongs() -> Result<String, Error> {
+    let url = "http://pingpong-svc:1235/pingpong";
+    let response = reqwest::get(url).await?;
+    let pongs = response.text().await?;
+    Ok(pongs)
 }
